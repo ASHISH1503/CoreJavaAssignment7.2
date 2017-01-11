85e074448243a764e6aa6abf66b95e955c5e8592
ASHISH1503 <ashishranjan988@gmail.com>
2017-01-11 21:06:27 +0530

    Add files via upload

---
+++ SubString.java
@@ -0,0 +1,11 @@
+
+public class SubString {
+
+	public static void main(String[] args) {
+		String S1 = "Acadgild";
+		System.out.println(S1.substring(2));
+		System.out.println(S1.substring(1, 3));
+
+	}
+
+}
